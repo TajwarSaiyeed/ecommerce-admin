555717993d8d28295ac5eb0eb99430d1556cc5af
TajwarSaiyeed <rjabid36@gmail.com>
2023-09-21 21:32:13 +0600

    product finished

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -132,7 +132,7 @@ export const ProductForm: React.FC<ProductFormProps> = ({initialData, categories
                         name={"images"}
                         render={({field}) => {
                             return <FormItem
-                                className={`${field.value ? "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-2" : "max-w-[300px]"}`}>
+                                className={`${field.value ? "flex flex-wrap items-center gap-2" : "max-w-[300px]"}`}>
                                 <FormControl>
                                     <FileUpload
                                         endpoint={'productImages'}
--- app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
@@ -4,7 +4,6 @@ import {toast} from "react-hot-toast";
 import axios from "axios";
 import {useState} from "react";
 import {useParams, useRouter} from "next/navigation";
-import {BillboardColumn} from "./columns";
 import {Button} from "@/components/ui/button";
 import {
     DropdownMenu,
@@ -15,9 +14,10 @@ import {
 } from "@/components/ui/dropdown-menu";
 import {Copy, Edit, MoreHorizontal, Trash} from "lucide-react";
 import {AlertModal} from "@/components/modals/alert-modal";
+import {ProductColumn} from "./columns";
 
 interface CellActionProps {
-    data: BillboardColumn
+    data: ProductColumn
 }
 
 export const CellAction: React.FC<CellActionProps> = ({data}) => {
@@ -28,18 +28,18 @@ export const CellAction: React.FC<CellActionProps> = ({data}) => {
 
     const onCopy = (id: string) => {
         navigator.clipboard.writeText(id)
-        toast.success("Billboard Id copied to the clipboard")
+        toast.success("Product Id copied to the clipboard")
     }
 
     const onDelete = async () => {
         try {
             setLoading(true);
-            await axios.delete(`/api/${params.storeId}/billboards/${data.id}`)
+            await axios.delete(`/api/${params.storeId}/products/${data.id}`)
             router.refresh()
-            router.push(`/${params.storeId}/billboards`)
+            router.push(`/${params.storeId}/products`)
             toast.success("Billboard deleted.")
         } catch (error) {
-            toast.error("Make sure you removed all categories using this billboard first.")
+            toast.error("Something went wrong")
         } finally {
             setLoading(false)
             setOpen(false)
@@ -68,7 +68,7 @@ export const CellAction: React.FC<CellActionProps> = ({data}) => {
                         <Copy className={'mr-2 h-4 w-4'}/> Copy Id
                     </DropdownMenuItem>
                     <DropdownMenuItem
-                        onClick={() => router.push(`/${params.storeId}/billboards/${data.id}`)}
+                        onClick={() => router.push(`/${params.storeId}/products/${data.id}`)}
                     >
                         <Edit className={'mr-2 h-4 w-4'}/> Update
                     </DropdownMenuItem>
