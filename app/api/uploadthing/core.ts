6bf14d2af8bf7ec93882cc89cef29ac06cc21c37
TajwarSaiyeed <rjabid36@gmail.com>
2023-09-21 20:06:21 +0600

    product | create | images fix | schema update

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -12,7 +12,10 @@ const handleAuth = () => {
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-    imageUrl:  f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    imageUrl:  f({ image: { maxFileSize: "4MB" } })
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => {}),
+    productImages: f({ image: { maxFileSize: "4MB", maxFileCount: 4 } })
         .middleware(() => handleAuth())
         .onUploadComplete(() => {}),
 } satisfies FileRouter;
--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -9,13 +9,15 @@ import {Button} from "@/components/ui/button";
 
 interface FileUploadProps {
     disabled?: boolean;
-    endpoint: "imageUrl";
-    onChange: (url?: string) => void;
-    value: string
+    endpoint: "imageUrl" | "productImages";
+    onChange?: (url?: string) => void
+    value?: string;
+    productImages?: string[];
+    onProductImagesChange?: (url: string[]) => void
 }
 
-export const FileUpload = ({onChange, value, endpoint}: FileUploadProps) => {
-    if (value) {
+export const FileUpload = ({onChange, value, endpoint, productImages, onProductImagesChange}: FileUploadProps) => {
+    if (value && endpoint === "imageUrl") {
         return (
             <div className={"relative h-[300px] w-[300px] rounded-md overflow-hidden"}>
                 <Image
@@ -29,7 +31,7 @@ export const FileUpload = ({onChange, value, endpoint}: FileUploadProps) => {
                 >
                     <Button
                         variant={'destructive'} size={'icon'}
-                        onClick={() => onChange("")}
+                        onClick={() => onChange ? onChange("") : null}
                         type={"button"}
                     >
                         <Trash className={"h-4 w-4"}/>
@@ -40,13 +42,46 @@ export const FileUpload = ({onChange, value, endpoint}: FileUploadProps) => {
     }
 
 
+    if (productImages && endpoint === 'productImages') {
+        return productImages.map((image, index) => (
+            <div key={index} className={"relative h-[300px] w-[300px] bg-zinc-400 rounded-md overflow-hidden"}>
+                <Image
+                    fill
+                    src={image}
+                    alt={"upload"}
+                    className={"rounded-md object-cover"}
+                />
+                <div
+                    className='absolute top-2 right-2 cursor-pointer z-10'
+                >
+                    <Button
+                        variant={'destructive'} size={'icon'}
+                        onClick={() => onProductImagesChange && onProductImagesChange(productImages.filter((image, i) => i !== index))}
+                        type={"button"}
+                    >
+                        <Trash className={"h-4 w-4"}/>
+                    </Button>
+                </div>
+            </div>
+        ))
+    }
+
+
     return (
         <UploadDropzone
             endpoint={endpoint}
             onClientUploadComplete={(res) => {
-                const imageUrl = res?.[0]?.url;
-                onChange(imageUrl);
+                if (endpoint === 'imageUrl') {
+                    const imageUrl = res?.[0]?.url;
+                    if (onChange) {
+                        onChange(imageUrl);
+                    }
+                } else if (endpoint === "productImages" && onProductImagesChange) {
+                    const imageUrls = res?.map((image) => image.url) || [];
+                    onProductImagesChange(imageUrls);
+                }
             }}
+
             onUploadError={(err: any) => {
                 console.error(err);
             }}
